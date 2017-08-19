cc19444a62832abdcffb53d0e495a78afaea3f81
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-19 23:51:32 +0200

    Deux nouvelles fonctions
    J'ai essayer de faire l'interface avec les currency code mais on ne comprenait rien. Alors j'ai récupéré les name dans un array pour les utiliser dans l'interface.
    Du coup, j'ai créé une deuxième fonction nameToCode pour récupérer le currency code en fonction du nom et l'afficher dans l'interface (tu verras ce que je veux dire si tu testes l'interface).

--- src/curonv/CurrencyInfo.java
+++ src/curonv/CurrencyInfo.java
@@ -10,13 +10,10 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class CurrencyInfo {
-
     public static String CurrencyName (String arg) {
-
         JSONParser parser = new JSONParser();
 
         try {
-        	
             JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
             JSONObject currencyId = (JSONObject) file.get(arg);
             
@@ -29,24 +26,68 @@ public class CurrencyInfo {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-	return "Error";
-
+		return "Error";
     }
     
-    public static ArrayList<String> CurrencyList () {
+    
+    public static String CurrencySymbol(String arg) {
+    	JSONParser parser = new JSONParser();
 
+        try {
+            JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
+            JSONObject currencyId = (JSONObject) file.get(arg);
+            return ((String) (currencyId.get("symbol_native")));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+		return "Null";	
+    }
+    
+    public static String nameToCode(String name) {
+		JSONParser parser = new JSONParser();
+
+        try {
+        	JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
+        	for (Object key : file.keySet()) {
+                String keyStr = (String)key;
+                JSONObject currencyId = (JSONObject) file.get(keyStr);
+                
+                if (((String) currencyId.get("name")).equals(name)) {
+                	return ((String) keyStr);
+                }
+            }
+            return null;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+		return "Error";		
+	}
+    
+	
+	public static ArrayList<String> CurrencyList () {
         JSONParser parser = new JSONParser();
         ArrayList<String> currencyList = new ArrayList<>();
 
         try {
-        	
             JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
             
             for (Object key : file.keySet()) {
                 String keyStr = (String)key;
-                currencyList.add(keyStr);
+                JSONObject currencyId = (JSONObject) file.get(keyStr);
+                currencyList.add((String) currencyId.get("name"));
             }
             
+            currencyList.sort(String::compareToIgnoreCase);
             return (ArrayList<String>) currencyList;
 
         } catch (FileNotFoundException e) {
@@ -57,26 +98,5 @@ public class CurrencyInfo {
             e.printStackTrace();
         }
 		return null;
-    }
-    
-    public static String CurrencySymbol(String arg) {
-        JSONParser parser = new JSONParser();
-
-        try {
-        	
-            JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
-            JSONObject currencyId = (JSONObject) file.get(arg);
-		
-            return ((String) (currencyId.get("symbol_native")));
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-		return "Null";
-    }
-	
+	}
 }
