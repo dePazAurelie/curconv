606af194ebff83dc864a2488d453b6cb8ccca8c8
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-19 23:41:42 +0200

    indentation :p

--- src/curonv/ConversionRate.java
+++ src/curonv/ConversionRate.java
@@ -3,33 +3,27 @@ package curonv;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ConversionRate {
-	
     public static void changeConversionRate (Currency currencyOrigin, Currency currencyTarget) {
+    	JSONParser parser = new JSONParser();
+    	
+        try {
+        	JSONObject file = (JSONObject) parser.parse(new FileReader("live.json"));
+            JSONObject quotes = (JSONObject) file.get("quotes");
 
-    		JSONParser parser = new JSONParser();
+            currencyOrigin.setConversionRateUSD(Double.parseDouble(quotes.get("USD" + currencyOrigin.getCurrencyCode()).toString()));
+            currencyTarget.setConversionRateUSD(Double.parseDouble(quotes.get("USD" + currencyTarget.getCurrencyCode()).toString()));
 
-            try {
-            	
-                JSONObject file = (JSONObject) parser.parse(new FileReader("live.json"));
-                JSONObject quotes = (JSONObject) file.get("quotes");
-
-                currencyOrigin.setConversionRateUSD(Double.parseDouble(quotes.get("USD" + currencyOrigin.getCurrencyCode()).toString()));
-                currencyTarget.setConversionRateUSD(Double.parseDouble(quotes.get("USD" + currencyTarget.getCurrencyCode()).toString()));
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
+        } catch (FileNotFoundException e) {
+        	e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
+    }
 }
-   
