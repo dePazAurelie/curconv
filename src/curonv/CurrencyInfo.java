6f9029eaa18367fdee02888eb8229cd1ff437fa3
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-19 15:34:50 +0200

    Update CurrencyInfo.java

--- src/curonv/CurrencyInfo.java
+++ src/curonv/CurrencyInfo.java
@@ -1,9 +1,9 @@
 package curonv;
 
 import java.io.FileNotFoundException;
-
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
 
 import org.json.simple.JSONObject;
@@ -30,19 +30,44 @@ public class CurrencyInfo {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-		return "Error";
+	return "Error";
 
     }
     
+    public static ArrayList<String> CurrencyList () {
+
+        JSONParser parser = new JSONParser();
+        ArrayList<String> currencyList = new ArrayList<>();
+
+        try {
+        	
+            JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
+            
+            for (Object key : file.keySet()) {
+                String keyStr = (String)key;
+                currencyList.add(keyStr);
+            }
+            
+            return (ArrayList<String>) currencyList;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+		return null;
+    }
+    
     public static String CurrencySymbol(String arg) {
-    		
         JSONParser parser = new JSONParser();
 
         try {
         	
             JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
             JSONObject currencyId = (JSONObject) file.get(arg);
-            
+		
             return ((String) (currencyId.get("symbol_native")));
 
         } catch (FileNotFoundException e) {
@@ -53,7 +78,6 @@ public class CurrencyInfo {
             e.printStackTrace();
         }
 		return "Null";
-    	
     }
 	
 }
