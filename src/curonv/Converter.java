42a22a43d2151dfc4440581bdcfbecdaf2488059
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-19 23:46:44 +0200

    Nouveau converteur
    Le converter appelle lui-même la fenêtre dans son builder.
    La fonction conversion gère en partie les cas d'erreur. Par ailleurs, elle prend à présent en argument trois String (et non plus un double et deux string) directement depuis l'interface et gère la conversion. Elle renvoie un String vers l'interface.
    Nouvelle fonction round pour arrondir les double.

--- src/curonv/Converter.java
+++ src/curonv/Converter.java
@@ -1,34 +1,64 @@
 package curonv;
 
+import java.awt.EventQueue;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Converter {
-
-    public void conversion(double nbOrigin, String strOrigin, String strTarget) {
-        double nbTarget;
+	
+    public static String conversion(String strOriginValue, String strOrigin, String strTarget) {
         double nbDollar;
-        Currency currencyOrigin = new Currency(strOrigin);
-        Currency currencyTarget = new Currency(strTarget);
+        double nbResult;
+        double nbOrigin = Double.parseDouble(strOriginValue);
+        String strResult = "";
         
-        ConversionRate.changeConversionRate(currencyOrigin, currencyTarget);
+        if (strOrigin.equals("Currency") || strTarget.equals("Currency")) {
+        	return "Please select two currencies !";
+        } else {
+        	Currency currencyOrigin = new Currency(strOrigin);
+        	Currency currencyTarget = new Currency(strTarget);
+        
+        	ConversionRate.changeConversionRate(currencyOrigin, currencyTarget);
 
-        nbDollar = dollarConvert(nbOrigin, currencyOrigin.getConversionRateUSD());
-        nbTarget = convertDollar(nbDollar, currencyTarget.getConversionRateUSD());
-
-        System.out.println(nbOrigin + " " + currencyOrigin.getSymbol() + " = " + nbDollar + " $ = " + nbTarget + " " + currencyTarget.getSymbol());
+        	nbDollar = dollarConvert(nbOrigin, currencyOrigin.getConversionRateUSD());
+        	nbResult = convertDollar(nbDollar, currencyTarget.getConversionRateUSD());
+        	
+        	nbResult = round(nbResult);
+        	
+        	strResult += nbResult;
+        	return strResult;
+        }
     }
 
-    public double dollarConvert(double nbOrigin, double rateUSD) {
+    public static double dollarConvert(double nbOrigin, double rateUSD) {
         double nbDollar;
         nbDollar = (nbOrigin/rateUSD);
         return nbDollar;
     }
 
-    public double convertDollar(double nbDollar, double USDrate) {
+    public static double convertDollar(double nbDollar, double USDrate) {
         double nbTarget;
         nbTarget = (nbDollar * USDrate);
         return nbTarget;
     }
+    
+    public static double round(double value) {
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
 
-    public Converter(double nbOrigin, String currencyOrigin, String currencyTarget) {
-        conversion(nbOrigin, currencyOrigin, currencyTarget);
+    public Converter() {      
+        EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Window frame = new Window();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+        
     }
 }
