7417dc943f99b7b9c34dc5725a4c4fa80bb9bc05
BuildTools <ashuvidz@gmail.com>
2017-08-19 18:54:20 +0200

    Correction imports

--- src/curonv/ConversionRate.java
+++ src/curonv/ConversionRate.java
@@ -7,7 +7,6 @@ import org.json.simple.parser.ParseException;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Set;
 
 public class ConversionRate {
 	
