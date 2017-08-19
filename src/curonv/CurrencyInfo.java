5e73f16e8a423de30ec9ef9b90fb820936881d2a
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-19 15:46:49 +0200

    Ajout array de currency code (dans CurrencyInfo)
    J'ai récupéré l'array de currency code et je commence l'interface. =)

--- src/curonv/CurrencyInfo.java
+++ src/curonv/CurrencyInfo.java
@@ -4,7 +4,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Set;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
