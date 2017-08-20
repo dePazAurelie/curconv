1caccb1747567e26869fb14b860a32f6e97b97ad
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-20 21:45:25 +0200

    + commentaires

--- src/curonv/Converter.java
+++ src/curonv/Converter.java
@@ -4,41 +4,85 @@ import java.awt.EventQueue;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+/**
+ * Classe qui gère la conversion d'une monnaie à une autre.
+ * 
+ * @author de Paz - Ronteix
+ */
+
 public class Converter {
 	
-    public static String conversion(String strOriginValue, String strOrigin, String strTarget) {
+	/**
+	 * Gère la conversion : prend en paramètre deux objets Currency. Récupère l'attribut strValue de originCurrency qui contient la valeur entrée par
+	 * l'utilisateur et la convertie en nombre de type double après gestion des cas d'erreur : regex = strValue ne doit contenir que des chiffres ou
+	 * des chiffres et un point.
+	 * 
+	 * Utilise l'attribut strAlertValue de Currency pour stocker les messages d'erreur et strValue pour stocker les valeurs initiale et finale.
+	 * 
+	 * @see Currency
+	 * 
+	 * @param originCurrency Monnaie d'origine
+	 * @param targetCurrency Monnaie target
+	 */
+	
+    public static void conversion(Currency originCurrency, Currency targetCurrency) {
+        originCurrency.setStrValue(originCurrency.getStrValue().replace(',', '.'));			// Remplace les virgules par un point
         double nbDollar;
         double nbResult;
-        double nbOrigin = Double.parseDouble(strOriginValue);
         
-        if (strOrigin.equals("Currency") || strTarget.equals("Currency")) {
-        	return "Please select two currencies !";
-        } else {
-        	Currency currencyOrigin = new Currency(strOrigin);
-        	Currency currencyTarget = new Currency(strTarget);
-        
-        	ConversionRate.changeConversionRate(currencyOrigin, currencyTarget);
-
-        	nbDollar = dollarConvert(nbOrigin, currencyOrigin.getConversionRateUSD());
-        	nbResult = convertDollar(nbDollar, currencyTarget.getConversionRateUSD());
+        // Test que l'utilisateur a bien entré un chiffre
+        if (!originCurrency.getStrValue().matches("[0-9]+.[0-9]+") && !originCurrency.getStrValue().matches("[0-9]+")) {
+        	targetCurrency.setStrAlertValue("Please, enter a numerical value !");
+        	targetCurrency.setStrValue("?");
         	
+        } else {
+        	double nbOrigin = Double.parseDouble(originCurrency.getStrValue());				// Conversion en double
+
+        	nbDollar = dollarConvert(nbOrigin, originCurrency.getConversionRateUSD());		// Converti en Dollar
+        	nbResult = convertDollar(nbDollar, targetCurrency.getConversionRateUSD());		// Converti dans la monnaie désirée.
         	nbResult = round(nbResult);
         	
-        	return String.format ("%.2f", nbResult);
+        	// Converti en String et remplace les virgules par un point
+        	targetCurrency.setStrValue(String.format ("%.2f", nbResult).replace(',', '.'));
+        	targetCurrency.setStrAlertValue("");
         }
     }
-
+    
+    /**
+     * Première conversion en Dollar : prend en paramètre le nombre (double nbOrigin) à convertir et le taux de conversion (double) entre la monnaie
+     * initiale et le Dollar. Renvoie le résultat de la convertion en Dollar (double nbDollar).
+     * 
+     * @param nbOrigin nombre à convertir
+     * @param rateUSD taux de conversion avec le Dollar
+     * @return nbDollar
+     */
     public static double dollarConvert(double nbOrigin, double rateUSD) {
         double nbDollar;
         nbDollar = (nbOrigin/rateUSD);
         return nbDollar;
     }
 
+    /**
+     * Deuxième conversion du Dollar vers la monnaie finale.
+     * prend en paramètre le nombre (double nbDollar) en Dollar à convertir et le taux de conversion (double) entre la monnaie finale et le Dollar.
+     * Renvoie le résultat de la convertion dans la monnaie finale (double nbTarget).
+     * 
+     * @param nbDollar Nombre en Dollar
+     * @param USDrate Taux de conversion entre le Dollar et la monnaie choisie
+     * @return nbTarget
+     */
     public static double convertDollar(double nbDollar, double USDrate) {
         double nbTarget;
         nbTarget = (nbDollar * USDrate);
         return nbTarget;
     }
+    
+    /**
+     * Arrondi à la deuxième décimale le nombre passé en paramètre.
+     * 
+     * @param value valeur à arrondir
+     * @return rounded value
+     */
     
     public static double round(double value) {
         BigDecimal bd = new BigDecimal(value);
@@ -46,8 +90,13 @@ public class Converter {
         return bd.doubleValue();
     }
 
+    /**
+     * Builer de Converter : Lance la fenêtre window pour interagir avec l'utilisateur.
+     * Ne prend pas de paramètre.
+     */
     public Converter() {      
         EventQueue.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				try {
 					Window frame = new Window();
