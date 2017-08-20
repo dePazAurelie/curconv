5f362b095f6e692ff7955d7c656a4e2b46b11a3b
dePazAurelie <30385374+dePazAurelie@users.noreply.github.com>
2017-08-20 21:48:28 +0200

    Update CurrencyInfo.java

--- src/curonv/CurrencyInfo.java
+++ src/curonv/CurrencyInfo.java
@@ -48,20 +48,29 @@ public class CurrencyInfo {
 		return "Null";	
     }
     
+    /**
+     * Fonction qui permet de trouver dans le fichier JSON le currency code à partir du nom de la monnaie.
+     * prend en paramètre le nom de la monnaie, renvoie un string currency code.
+     * Utilisé dans Window pour mettre à jour les JLabel.
+     * 
+     * @see Window
+     * @param name nom de la monnaie
+     * @return String keyStr key qui correspond au nom : Currency code
+     */
     public static String nameToCode(String name) {
 		JSONParser parser = new JSONParser();
 
         try {
-            JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
-            for (Object key : file.keySet()) {
-            String keyStr = (String)key;
-            JSONObject currencyId = (JSONObject) file.get(keyStr);
+        	JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
+        	for (Object key : file.keySet()) {
+                String keyStr = (String)key;
+                JSONObject currencyId = (JSONObject) file.get(keyStr);
                 
-            if (((String) currencyId.get("name")).equals(name)) {
-              	return ((String) keyStr);
+                if (((String) currencyId.get("name")).equals(name)) {
+                	return (keyStr);
+                }
             }
-        }
-        return null;
+            return null;
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -70,10 +79,16 @@ public class CurrencyInfo {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-	return "Error";		
-    }
+		return "Error";		
+	}
     
-	
+    /**
+     * Fonction qui créé un array de tous les noms de monnaie contenu dans le fichier JSON
+     * Utilisé dans Window pour mettre à jour les Combobox.
+     *
+     * @see Window
+     * @return un array de String qui contient tous les noms de monnaie issus du fichier JSON
+     */
 	public static ArrayList<String> CurrencyList () {
         JSONParser parser = new JSONParser();
         ArrayList<String> currencyList = new ArrayList<>();
@@ -81,14 +96,14 @@ public class CurrencyInfo {
         try {
             JSONObject file = (JSONObject) parser.parse(new FileReader("money-info.json"));
             
-            for (Object key : file.keySet()) {
+            for (Object key : file.keySet()) {				// Boucle sur toutes les key et enregistre le nom dans un array
                 String keyStr = (String)key;
                 JSONObject currencyId = (JSONObject) file.get(keyStr);
                 currencyList.add((String) currencyId.get("name"));
             }
             
             currencyList.sort(String::compareToIgnoreCase);
-            return (ArrayList<String>) currencyList;
+            return currencyList;
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -97,6 +112,6 @@ public class CurrencyInfo {
         } catch (ParseException e) {
             e.printStackTrace();
         }
-	return null;
-    }
+		return null;
+	}
 }
