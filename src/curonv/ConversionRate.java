a5af6adbac21608712577861df2d151174490bfe
VyrtualSynthese <vyrtualsynthese@gmail.com>
2017-08-21 16:42:42 +0200

    Ajout du pull update API

--- src/curonv/ConversionRate.java
+++ src/curonv/ConversionRate.java
@@ -3,9 +3,15 @@ package curonv;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class ConversionRate {
 	
@@ -27,4 +33,36 @@ public class ConversionRate {
             e.printStackTrace();
         }
     }
+    
+    public static void updateConversionRate () {
+        JSONParser parser = new JSONParser();
+
+        try {         
+            URL oracle = new URL("http://apilayer.net/api/live?access_key=f63bce189d18f99578accd94d5ecda6a"); // URL to Parse
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            
+            String str = "";
+            String buff = "";
+            while ((buff = in.readLine()) != null) {
+            		str += buff;
+            }
+           in.close();
+            System.out.println(str);
+            
+            try (FileWriter file = new FileWriter("live.json")) {
+
+                file.write(str);
+                file.flush();
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+    } catch (FileNotFoundException e) {
+        e.printStackTrace();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }   
+}
 }
